12c286b4ab9db38aaff68977da37a1f7211d2e4f
eduardofmarcos <eduardomarcos_@outlook.com>
2020-07-23 22:27:43 -0300

    possible moves of a piece - part1

--- chess-system/src/boardgame/Piece.java
+++ chess-system/src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 	
 	protected Position position;
 	private Board board;
@@ -15,6 +15,29 @@ public class Piece {
 
 	protected Board getBoard() {
 		return board;
+	}
+	
+	public abstract boolean[][] possibleMoves();
+	
+	
+	/****************** Implementação concreta que depende de metodos abstratos************/
+	public boolean possibleMove(Position position) {
+		//hook methods um metodo que faz um gancho com a subclasse
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	};
+	
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+
+	
+		for(int i=0; i<mat.length;i++) {
+			for (int j=0; j<mat.length;j++) {
+				if(mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 	
 	
