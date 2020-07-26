3b58dfd4f251c607bc444226e0b93511cacb695e
eduardofmarcos <eduardomarcos_@outlook.com>
2020-07-26 19:43:27 -0300

    implemented promotion adjustments

--- chess-system/src/chess/ChessMatch.java
+++ chess-system/src/chess/ChessMatch.java
@@ -1,12 +1,12 @@
 package chess;
 
 
-import java.security.InvalidParameterException;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import javax.imageio.metadata.IIOInvalidTreeException;
+
 
 
 
@@ -155,7 +155,7 @@ public class ChessMatch {
 		}
 		
 		if(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
-			throw new InvalidParameterException("Invalid type for promotion!");
+			return promoted;
 		}
 		
 		Position pos = promoted.getChessPosition().toPosition();
